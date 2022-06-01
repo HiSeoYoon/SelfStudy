fcce672d2587495deb47837e1c867ee481deca6b
sy <hi.seoyoon22@gmail.com>
2022-06-01 16:20:29 +0900

    Make hello.servlet spring web project

---
+++ Spring_Inflearn_MVC/src/main/java/com/example/spring_inflearn_mvc/SpringInflearnMvcApplication.java
@@ -0,0 +1,25 @@
+package com.example.spring_inflearn_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLOutput;
+
+import static java.lang.System.*;
+
+@SpringBootApplication
+public class SpringInflearnMvcApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringInflearnMvcApplication.class, args);
+
+
+        out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        out.println("김유철 김유철 김유철 김유철 김유철");
+        out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+        out.println("hello");
+    }
+
+}
